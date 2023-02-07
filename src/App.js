5611df5d6dd2ba7a780dd0ce2067386be6a95f30
Marlon Leon <ghostveene@gmail.com>
2023-02-07 09:37:57 -0500

    rendering die component 10 times with value prop

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import Die from "./Components/Die"
+
 export default function App() {
     return (
         <main>
-
+            <Die 
+                value={1}
+            />
+            <Die 
+                value={5}
+            />
+            <Die 
+                value={4}
+            />
+            <Die 
+                value={3}
+            />
+            <Die 
+                value={2}
+            />
+            <Die 
+                value={6}
+            />
+            <Die 
+                value={5}
+            />
+            <Die 
+                value={4}
+            />
+            <Die 
+                value={1}
+            />
+            <Die 
+                value={2}
+            />
         </main>
     )
 }
