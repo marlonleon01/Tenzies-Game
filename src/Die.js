26b9891970a1a4c3b67662c6f0f4e608eeae1848
Marlon Leon <ghostveene@gmail.com>
2023-02-07 11:14:47 -0500

    added styles

--- src/Die.js
+++ src/Die.js
@@ -1,6 +1,10 @@
 export default function Die(prop) {
+    const styles = {
+        backgroundColor: prop.isHeld ? "#59E391" : "#FFF"
+    }
+    
     return (
-        <div className="die-face">
+        <div className="die-face" style={styles}>
             <h2 className="die-num">{prop.value}</h2>
         </div>
     )
